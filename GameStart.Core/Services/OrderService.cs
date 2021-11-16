7780a678192aa01ee22fd6350205b507f752b5f7
DonOllario <oliver.stenstrom@hotmail.com>
2021-11-16 22:51:24 +0100

    Core is now prepared except for OrderService

--- GameStart.Core/Services/OrderService.cs
+++ GameStart.Core/Services/OrderService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using GameStart.Core.Entities;
+using GameStart.Core.Entities.OrderAggregate;
+using GameStart.Core.Entities.ShoppingCartAggregate;
+using GameStart.Core.Interfaces;
+using GameStart.Core.Specifications;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,39 @@ using System.Threading.Tasks;
 
 namespace GameStart.Core.Services
 {
-    internal class OrderService
+    public class OrderService : IOrderService
     {
+        private readonly IRepository<Order> _orderRepository;
+        private readonly IRepository<ShoppingCart> _shoppingCartRepository;
+        private readonly IRepository<Product> _productRepository;
+
+        public OrderService(IRepository<Order> orderRepository,
+            IRepository<ShoppingCart> shoppingCartRepository,
+            IRepository<Product> productRepository)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCartRepository = shoppingCartRepository;
+            _productRepository = productRepository;
+        }
+
+        public async Task CreateOrderAsync(int shoppingCartId, string address)
+        {
+            var shoppingCartSpec = new ShoppingCartWithItemsSpecification(shoppingCartId);
+            var shoppingCart = await _shoppingCartRepository.GetBySpecAsync(shoppingCartSpec);
+
+            var productsSpecification = new ProductsSpecification(shoppingCart.Items.Select(item => item.ProductId).ToArray());
+            var products = await _productRepository.ListAsync(productsSpecification);
+
+            var items = shoppingCart.Items.Select(shoppingCartItem =>
+            {
+                var product = products.First(p => p.Id == shoppingCartItem.ProductId);
+                var orderItem = new OrderItem(shoppingCartItem.UnitPrice, shoppingCartItem.Quantity);
+                return orderItem;
+            }).ToList();
+
+            var order = new Order(shoppingCart.UserId, address, items);
+
+            await _orderRepository.AddAsync(order);
+        }
     }
 }
