bd0cc7258e44b5ca2e6813fe5c3c843413c22357
Kevin Dani <Kevin.Dani@sigma.se>
2021-11-17 16:17:10 +0100

    Working on setting up database configurations

--- GameStart.Infrastructure/Data/ShoppingCartQueryService.cs
+++ GameStart.Infrastructure/Data/ShoppingCartQueryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GameStart.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace GameStart.Infrastructure.Data
 {
-    internal class ShoppingCartQueryService
+    public class ShoppingCartQueryService : IShoppingCartQueryService
     {
+        private readonly GameStartContext _dbContext;
+
+        public ShoppingCartQueryService(GameStartContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> CountTotalBasketItems(string username)
+        {
+            var totalItems = await _dbContext.ShoppingCarts
+                .Where(ShoppingCart => ShoppingCart.UserId == username)
+                .SelectMany(item => item.Items)
+                .SumAsync(sum => sum.Quantity);
+
+            return totalItems;
+        }
     }
 }
---
+++ GameStart.Infrastructure/Data/StartupSetup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStart.Infrastructure.Data
+{
+    public static class StartupSetup
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<GameStartContext>(options =>
+            options.UseSqlServer(connectionString));
+        }
+    }
+}
