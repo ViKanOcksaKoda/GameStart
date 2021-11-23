0672c73a3a344ba3e9e3987b8bffca189254c7cd
Kevin Dani <Kevin.Dani@sigma.se>
2021-11-23 15:08:48 +0100

    Created the basics for logging in, needs a few improvements. Also added a / in products/listbycategory

---
+++ GameStart/Endpoints/Users/Login.cs
@@ -0,0 +1,41 @@
+﻿using Ardalis.ApiEndpoints;
+using GameStart.Core.Entities;
+using GameStart.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace GameStart.Endpoints.Users
+{
+    public class Login : BaseAsyncEndpoint
+    .WithRequest<LoginUserRequest>
+    .WithResponse<LoginUserResponse>
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public Login(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        [HttpGet("api/users/login{UserName}/{Password}")]
+        [SwaggerOperation(
+           Summary = "Login as user",
+           Description = "login with username and password",
+           OperationId = "users.login",
+           Tags = new[] { "User Endpoints" })
+        ]
+        public override async Task<ActionResult<LoginUserResponse>> HandleAsync([FromRoute]LoginUserRequest request, CancellationToken cancellationToken = default)
+        {
+            var response = new LoginUserResponse();
+            var users = await _userRepository.ListAsync();
+            for ( int i = 0; i < users.Count; i++)
+            {
+                if(users[i].UserName == request.UserName && users[i].Password == request.Password)
+                {
+                    response.LoggedIn = true;
+                }
+            }
+
+            return response;
+        }
+    }
+}
