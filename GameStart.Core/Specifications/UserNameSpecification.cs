74b146243237b0c58945437ae7ed20264ff486b7
DonOllario <oliver.stenstrom@hotmail.com>
2021-11-24 16:18:57 +0100

    Added username Exception

---
+++ GameStart.Core/Specifications/UserNameSpecification.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.Specification;
+using GameStart.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStart.Core.Specifications
+{
+    public class UserNameSpecification : Specification<User>
+    {
+        public UserNameSpecification(string userName)
+        {
+            Query.Where(name => userName == name.UserName);
+        }
+    }
+}
---
+++ UnitTests.csproj/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace UnitTests.csproj
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
