f73b5b76079e98d7a667fa94fb49605257ee3e14
DonOllario <oliver.stenstrom@hotmail.com>
2021-11-28 19:30:06 +0100

    Finished all unit tests

---
+++ UnitTests.csproj/GameStartCore/Specifications/ProductsNameSpecification.cs
@@ -0,0 +1,41 @@
+﻿using GameStart.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.GameStartCore.Specifications
+{
+    public class ProductsNameSpecification
+    {
+        [Theory]
+        [InlineData(null, 0)]
+        [InlineData("Valheim", 1)]
+        [InlineData("New World", 1)]
+        [InlineData("", 0)]
+        [InlineData("Call of Duty", 0)]
+        public void MatchesExpectedNumberOfProducts(string productName, int expectedCount)
+        {
+            var spec = new GameStart.Core.Specifications.ProductNameSpecification(productName);
+
+            var result = GetTestItemCollection()
+                .AsQueryable()
+                .Where(spec.WhereExpressions.FirstOrDefault());
+
+            Assert.Equal(expectedCount, result.Count());
+        }
+
+        public List<Product> GetTestItemCollection()
+        {
+            return new List<Product>()
+            {
+                new Product(1, "Valheim", "Description", 0, 0),
+                new Product(2, "New World", "Description", 0, 0),
+                new Product(2, "Zelda", "Description", 0, 0),
+                new Product(3, "WoW", "Description", 0, 0),
+            };
+        }
+    }
+}
---
+++ UnitTests.csproj/GameStartCore/Specifications/ProductsSpecification.cs
@@ -0,0 +1,58 @@
+﻿using GameStart.Core.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.GameStartCore.Specifications
+{
+    public class ProductsSpecification
+    {
+        [Fact]
+        public void MatchesSpecificProduct()
+        {
+            var productIds = new int[] { 1 };
+            var spec = new GameStart.Core.Specifications.ProductsSpecification(productIds);
+
+            var result = GetTestCollection()
+                .AsQueryable()
+                .Where(spec.WhereExpressions.FirstOrDefault());
+
+            Assert.NotNull(result);
+            Assert.Single(result.ToList());
+        }
+
+        [Fact]
+        public void MatchesAllProducts()
+        {
+            var productIds = new int[] { 1, 3 };
+            var spec = new GameStart.Core.Specifications.ProductsSpecification(productIds);
+
+            var result = GetTestCollection()
+                .AsQueryable()
+                .Where(spec.WhereExpressions.FirstOrDefault());
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.ToList().Count);
+        }
+
+        private List<Product> GetTestCollection()
+        {
+            var products = new List<Product>();
+
+            var mockProduct1 = new Mock<Product>(1, "Product 1 description", "Product 1", 1.5m, 2);
+            mockProduct1.SetupGet(x => x.Id).Returns(1);
+
+            var mockProduct3 = new Mock<Product>(3, "Product 3 description", "Product 3", 3.5m, 2);
+            mockProduct3.SetupGet(x => x.Id).Returns(3);
+
+            products.Add(mockProduct1.Object);
+            products.Add(mockProduct3.Object);
+
+            return products;
+        }
+    }
+}
